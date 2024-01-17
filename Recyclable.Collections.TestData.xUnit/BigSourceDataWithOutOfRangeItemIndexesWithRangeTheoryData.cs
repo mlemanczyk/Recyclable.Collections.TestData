bb34e8893e1312748a815675495d6bb2090a0730
mlemanczyk <marek@lemanczyk.eu>
2024-01-17 21:39:00 +0100

    BigSourceDataWithOutOfRangeItemIndexesWithRangeTheoryData added.

---
+++ Recyclable.Collections.TestData.xUnit/BigSourceDataWithOutOfRangeItemIndexesWithRangeTheoryData.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Recyclable.Collections.TestData.xUnit
+{
+	public class BigSourceDataWithOutOfRangeItemIndexesWithRangeTheoryData : TheoryData<string, IEnumerable<long>, long, int, IEnumerable<(long, long, long)>>
+	{
+		public BigSourceDataWithOutOfRangeItemIndexesWithRangeTheoryData()
+		{
+			foreach (var tc in RecyclableLongListTestData.BigSourceDataWithOutOfRangeItemIndexesWithRangeVariants)
+			{
+				Add(tc.TestCase, tc.TestData, tc.ItemsCount, tc.BlockSize, tc.ItemIndexesWithRange);
+			}
+		}
+	}
+}
